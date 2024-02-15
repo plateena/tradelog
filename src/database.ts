b969980c0dbf6f31bb79c8a1b038c613ae20b9a2
zainundin <zaiundin.coders@gmail.com>
2024-02-16 00:00:05 +0800

    feat: add mongodb and db connection

---
+++ src/database.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+
+let connection: mongoose.Mongoose
+
+export async function connect(): Promise<void> {
+    try {
+        if (!connection) {
+            connection = await mongoose.connect(
+                "mongodb://mongodb:27017/tradelog",
+                {
+                    user: "root",
+                    pass: "example",
+                    authSource: "admin",
+                    maxPoolSize: 10,
+                }
+            )
+
+            await new Promise<void>((res, rej) => {
+                mongoose.connection.once("connected", () => {
+                    res()
+                })
+                mongoose.connection.on("error", (err) => {
+                    rej(err)
+                })
+            })
+        }
+    } catch (error) {
+        console.error("error connecting to mongodb:", error)
+        throw error
+    }
+}
+
+export function getConnection(): mongoose.Connection {
+    if (!connection) {
+        throw new Error("database connection has not been established.")
+    }
+    return connection.connection
+}
+
+export const close = async () => {
+    if (!connection) {
+        throw new Error("database connection has not been established.")
+    }
+    return connection.connection.close()
+}
+
+connect()
+
+export default { connect, getConnection, close }
---
+++ src/tests/db-connection.test.ts
@@ -0,0 +1,17 @@
+import db from "./../database"
+
+/**
+ * @group db
+ */
+
+describe("DB", () => {
+
+    afterAll(() => {
+        db.close()
+    })
+
+    it("can connect to database", async () => {
+        await db.connect()
+        expect(db.getConnection().readyState).toBe(1)
+    }, 10000);
+});
